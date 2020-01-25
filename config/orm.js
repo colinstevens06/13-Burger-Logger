e02b9c9d9030bb5abf9e83a4b09e78263b612e6d
Colin Stevens <colinstevens06@gmail.com>
2020-01-25 18:41:44 -0500

    added schema

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,21 @@
 // Connection to mysql
 const connection = require("../config/connection");
 
+// in case I need to use question marks
+function printQuestionMarks(num) {
+   var arr = [];
+ 
+   for (var i = 0; i < num; i++) {
+     arr.push("?");
+   }
+ 
+   return arr.toString();
+ }
+
+
+
 const orm = {
-   all: function(tableInput, callback)
+   all: function(tableInput, callback) {
+      let queryString = "SELECT * FROM "
+   }
 };
