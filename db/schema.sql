067dd2f8501b0006cbda0a17cbcddfc19a382c03
Colin Stevens <colinstevens06@gmail.com>
2020-01-25 18:44:26 -0500

    updated schema - minor edit

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
+DROP DATABASE IF EXISTS burger_db;
+
 CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers (
    id INT NOT NULL AUTO_INCREMENT,
-   text VARCHAR(255) NUT NULL,
+   burgerName VARCHAR(155) NOT NULL,
+   burgerToppings VARCHAR(355) NOT NULL,
    eaten BOOLEAN DEFAULT false,
    PRIMARY KEY (id)
 )
